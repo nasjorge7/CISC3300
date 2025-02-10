d2556b33308ee577de04fb91a4be0cbe9fa7965b
nasjorge7 <nasir.jorge7@gmail.com>
2025-02-10 17:35:20 -0500

    Added inclass-8.js

---
+++ inclass-8.js
@@ -0,0 +1,43 @@
+let result = "5" + 3;
+console.log(result); // Output = 53
+
+if (result === "53") {
+    console.log("Js treats the numbers like strings.");
+} else {
+    console.log("Something went wrong.");
+}
+
+let numTrue = Number(true);
+let numFalse = Number(false);
+
+if (numTrue === 1) {
+    console.log("true = 1");
+} else {
+    console.log("true did not convert.");
+}
+
+if (numFalse === 0) {
+    console.log("false becomes 0");
+} else {
+    console.log("false did not convert.");
+}
+
+if (Boolean(1)) {
+    console.log("Is true");
+} else {
+    console.log("Is false");
+}
+
+if (Boolean(0)) {
+    console.log("Is true");
+} else {
+    console.log("Is false");
+}
+
+if (Boolean("0")) {
+    console.log("'0' (string) is true");
+} else {
+    console.log("'0' (string) is false");
+}
+
+
